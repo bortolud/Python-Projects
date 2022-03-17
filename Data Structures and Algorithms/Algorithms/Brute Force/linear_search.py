e3ff700cf6c8986ad19f18f8ae1e5bf68b15048a
Daniel Bortolussi <daniel.bortolussi97@gmail.com>
2022-03-17 14:02:27 -0400

    Linear search and Tree structure

---
+++ Data Structures and Algorithms/Algorithms/Brute Force/linear_search.py
@@ -0,0 +1,24 @@
+#base case
+def linear_search(search_list, target_value):
+  for idx in range(len(search_list)):
+    if search_list[idx] == target_value:
+      return idx
+  raise ValueError("{0} not in list".format(target_value))
+
+#multiple matches
+def linear_search_multi(search_list, target_value):
+  matches = []
+  for idx in range(len(search_list)):
+    if search_list[idx] == target_value:
+      matches.append(idx)
+  if matches:
+    return matches
+  raise ValueError("{0} not in list".format(target_value))
+
+#find max lin search
+def find_max(search_list):
+  maximum_score_index = None
+  for idx in range(len(search_list)):
+    if not maximum_score_index or search_list[idx] > search_list[maximum_score_index]:
+      maximum_score_index = idx
+  return maximum_score_index
